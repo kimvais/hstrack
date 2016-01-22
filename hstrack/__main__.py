412223967fd7e0e11fcc591342a35e1b3751b136
Kimmo Parviainen-Jalanko <kimvais@ssh.com>
2016-01-22 10:17:26 +0200

    Parsing a few more tags.

--- hstrack/__main__.py
+++ hstrack/__main__.py
@@ -10,27 +10,38 @@ import os
 
 import click
 
+from hstrack.parse import Parser
+
 LOG_PATH = '/Applications/Hearthstone/Logs/Power.log'
 
 
 @click.command()
-def main():
-    logging.basicConfig(level=logging.DEBUG)
+@click.option('--verbose', '-v', count=True)
+def main(verbose):
+    logging.basicConfig(level=50 - (10 * verbose))
     logger = logging.getLogger(__name__)
     fd = os.open(LOG_PATH, os.O_RDONLY)
-    lines = list()
+    lines = list()   # XXX: We only have a single partial line, not a list.
     logger.info("Seeked to {}".format(os.lseek(fd, 0, os.SEEK_END)))
+
+    parser = Parser()
 
     def reader():
         while True:
-            buf = os.read(fd, 1024)
-            logger.info(buf)
+            buf = os.read(fd, 1024).decode('utf-8')
+            logger.debug(buf)
             if not buf:
+                logger.debug(os.fstat(fd))
                 break
             else:
                 x = buf.splitlines()
-                if lines and not lines[-1].endswith(b'\n'):
+                if lines and not lines[-1].endswith('\n'):
                     x[0] = lines.pop(-1) + x[0]
+                for line in x:
+                    if not line.endswith('\n'):
+                        lines.append(line)
+                    else:
+                        parser.process(line)
 
     loop = asyncio.get_event_loop()
     loop.add_reader(fd, reader)
--- hstrack/parse.py
+++ hstrack/parse.py
@@ -4,6 +4,8 @@
 # Release to public under MIT License, see LICENSE for details.
 #
 import logging
+import re
+from collections import defaultdict
 from pprint import pprint
 
 import click
@@ -12,10 +14,26 @@ LOG_PATH = '/Applications/Hearthstone/Logs/Power.log'
 
 logger = logging.getLogger(__name__)
 
+DEBUG_LINE_RE = re.compile(
+        r'^D (?P<ts>[^ ]+) (?P<module>[\w]+\.[\w]+)\(\) - (?P<msg>.+)')
+
+TAG_CHANGE_RE = re.compile(r'\W+ TAG_CHANGE Entity=(?P<entity>.+) tag=(?P<tag>.+) value=(?P<value>.+)')
+
+SHOW_ENTITY_RE = re.compile(r'\W+ SHOW_ENTITY Entity - Updating Entity=(?P<entity>.+) CardID=(?P<card_id>.+)')
+
+
+def chunks(l, n=2):
+    """Yield successive n-sized chunks from l.
+    :param l: iterable to be chunked
+    :param n: chunk size
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 
 class Game(object):
     def __init__(self):
-        pass
+        self.entities = defaultdict(dict)
 
 
 class Parser:
@@ -24,8 +42,22 @@ class Parser:
         self.games = list()
 
     def process(self, line):
-        if line.strip() == 'CREATE_GAME':
+        m = DEBUG_LINE_RE.match(line)
+        if m is None:
+            return
+        module = m.group('module')
+        if module != 'PowerTaskList.DebugPrintPower':
+            return
+        msg = m.group('msg')
+        logger.debug(msg)
+        if msg.strip() == 'CREATE_GAME':
             self.new_game()
+        elif msg.lstrip().startswith('TAG_CHANGE'):
+            self.tag_change(msg)
+        elif msg.lstrip().startswith('SHOW_ENTITY'):
+            self.show_entity(msg)
+        else:
+            print(line)
 
     def new_game(self):
         game = Game()
@@ -34,14 +66,33 @@ class Parser:
             self.games.append(self.current_game)
         self.current_game = game
 
-    def parse(self):
-        with open(LOG_PATH) as f:
-            for line in f:
-                if 'PowerTaskList.DebugPrintPower()' not in line:
-                    continue
-                beef = line.split(' - ', 1)[-1].rstrip()
-                logger.debug(beef)
-                self.process(beef)
+    def tag_change(self, msg):
+        m = TAG_CHANGE_RE.match(msg)
+        if m is None:
+            logger.error("Could not parse tag change: {}".format(msg))
+            return
+        entity, tag, value = self.get_entity(m)
+
+    def get_entity(self, m):
+        d = m.groupdict()
+        # logger.info(d)
+        entity = d['entity']
+        if entity.startswith('['):
+            entity = self.parse_entity(entity)
+        tag = d['tag']
+        value = d['value']
+        logger.info("{} {}={}".format(entity, tag, value))
+        return entity, tag, value
+
+    def parse_entity(self, entity):
+        return dict(chunks(re.split(' ?([\w]+)=', entity[1:-1])[1:]))
+
+    def show_entity(self, msg):
+        m = SHOW_ENTITY_RE.match(msg)
+        if m is None:
+            logger.error("Could not parse show entity: {}".format(msg))
+            return
+        entity, tag, value = self.get_entity(m)
 
 
 @click.command()
@@ -49,7 +100,9 @@ class Parser:
 def main(verbose):
     logging.basicConfig(level=50 - (10 * verbose))
     parser = Parser()
-    parser.parse()
+    with open(LOG_PATH) as f:
+        for line in f:
+            parser.process(line)
     pprint(parser.games)
 
 
